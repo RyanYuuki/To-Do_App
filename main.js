e09a20adf81f83659d7982168d18b7bb38d36cfc
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-05-06 09:52:16 +0530

    UI: Improved UI

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const inputElmt = document.getElementById("input");
-const TaskSample = document.getElementsByClassName("Task").innerHTML;
+const TaskSample = `
+<div class="Task">
+<div class="checkBox"><ion-icon name="checkmark-outline"></ion-icon></div>
+<div class="sampleTask">${inputElmt.value}</div>
+<div class="removeTask"><ion-icon name="close-outline"></ion-icon></div>
+</div>
+`
 const Tasks = document.getElementsByClassName("Tasks")[0];
 let index = 0;
 function createTask()
 {
     index++;
     Tasks.innerHTML += TaskSample;
-    Tasks.children[1].textContent = inputElmt.value;
 }
