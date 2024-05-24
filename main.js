ac6c4f0e5ce589617895c7bef7906bd4f17ab8df
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-05-24 22:43:08 +0530

    - Major : Updated the Whole UI & Logic

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const inputElmt = document.getElementById("input");
-const TaskSample = `
-<div class="Task">
-<div class="checkBox"><ion-icon name="checkmark-outline"></ion-icon></div>
-<div class="sampleTask">${inputElmt.value}</div>
-<div class="removeTask"><ion-icon name="close-outline"></ion-icon></div>
-</div>
-`
-const Tasks = document.getElementsByClassName("Tasks")[0];
+const Task = document.getElementsByClassName("Task");
+const TasksElmt = document.getElementsByClassName("Tasks")[0];
+const checkBox = document.getElementsByClassName("checkBox");
+const removeTask = document.getElementsByClassName("removeTask");
 let index = 0;
-function createTask()
-{
-    index++;
-    Tasks.innerHTML += TaskSample;
+function createTask() {
+    if (inputElmt.value == "") {
+        window.alert("Please enter a task name!");
+    }
+    else {
+        const TaskSample = `
+        <div class="Task">
+            <div class="checkBox"><ion-icon name="checkmark-outline"></ion-icon></div>
+            <div class="sampleTask">${inputElmt.value}</div>
+            <div class="removeTask"><ion-icon name="close-outline"></ion-icon></div>
+        </div>`;
+        TasksElmt.innerHTML += TaskSample;
+        inputElmt.value = '';
+    }
+    for (let i = 0; i < removeTask.length; i++) {
+        removeTask[i].addEventListener("click", () => {
+            TasksElmt.removeChild(TasksElmt.children[i]);
+        });
+    }
+    for (let i = 0; i < checkBox.length; i++) {
+        checkBox[i].addEventListener("click", () => {
+            Task[i].style.textDecoration = "line-through";
+            Task[i].style.opacity = "0.5";
+            Task[i].style.pointerEvents = "none";
+            checkBox[i].style.backgroundColor = "rgb(0,0,0,0.5)";
+        });
+    }
 }
